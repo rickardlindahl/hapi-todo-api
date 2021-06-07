b70f019e2aae56e09b85b425c9dd3c7c1132935f
Rickard Lindahl <rickard.lindahl@realsprint.com>
2021-06-07 22:39:21 +0200

    Add boilerplate routes for mvp

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -13,6 +13,52 @@ server.route({
   },
 });
 
+// Get todos
+server.route({
+  method: "GET",
+  path: "/todos",
+  handler: (_request, _h) => {
+    return "Todos!";
+  },
+});
+
+// Add todos to a list,
+server.route({
+  method: "POST",
+  path: "/todos",
+  handler: (_request, _h) => {
+    return "New todo created";
+  },
+});
+
+// Remove todos from the list,
+server.route({
+  method: "DELETE",
+  path: "/todos/{todoId}",
+  handler: (_request, _h) => {
+    return "Todo deleted";
+  },
+});
+
+// Mark todo as done,
+// Unmark todos as done,
+server.route({
+  method: "PATCH",
+  path: "/todos/{todoId}",
+  handler: (_request, _h) => {
+    return "Todo updated";
+  },
+});
+
+// Retrieve all events associated with a list of todos (think CRUD events,)
+server.route({
+  method: "GET",
+  path: "/events",
+  handler: (_request, _h) => {
+    return "All events!";
+  },
+})
+
 export const init = async () => {
   await server.initialize();
   return server;
